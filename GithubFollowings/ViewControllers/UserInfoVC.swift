6ae5106a4658775f5ce1f2baa8c90a5b3b5adf07
ilhan ipek <aliilhanipek92@gmail.com>
2025-02-01 19:05:39 +0300

    created: add function for adding any UIViewController to UIView and super class UIViewController

--- GithubFollowings/ViewControllers/UserInfoVC.swift
+++ GithubFollowings/ViewControllers/UserInfoVC.swift
@@ -9,23 +9,49 @@ import UIKit
 
 class UserInfoVC: UIViewController {
 
+  var headerView = UIView()
   var username: String!
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     view.backgroundColor = .systemBackground
-
     let doneButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismissVC))
     navigationItem.rightBarButtonItem = doneButton
+
+    layoutUI()
   }
 
   private func getFollowingInfo(username: String) {
     showLoadingView()
-    
   }
 
   @objc func dismissVC() {
     dismiss(animated: true)
+  }
+
+  func layoutUI() {
+    addSubviews()
+
+    headerView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      headerView.heightAnchor.constraint(equalToConstant: 180)
+    ])
+  }
+
+  func add(childVC: UIViewController, to containerView: UIView) {
+    addChild(childVC)
+    containerView.addSubview(childVC.view)
+    childVC.view.frame = containerView.bounds
+    childVC.didMove(toParent: self)
+  }
+
+  private func addSubviews() {
+    addSubviews(uiViews: [
+      headerView
+    ])
   }
 }
